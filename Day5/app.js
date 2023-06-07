75b0a70f0933d664fcfdf1f0d181c1953a8f8501
CodeFFun <sharmasanketa899@gmail.com>
2023-06-07 18:32:45 +0545

    Day5

---
+++ Day5/app.js
@@ -0,0 +1,8 @@
+const name = 'Sanket'
+const email = 'ss@gmail.com'
+const address = 'kathmandu'
+const phone = Symbol('abc')
+
+// console.log(`My name is ${name}, email is ${email}, adress ${address}, phone number:${phone}`)
+
+console.log(phone)
