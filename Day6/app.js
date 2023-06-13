40aa64855e482fe78529b11d59a38f842c371ae5
CodeFFun <sharmasanketa899@gmail.com>
2023-06-13 18:28:29 +0545

    Day5

--- Day6/app.js
+++ Day6/app.js
@@ -1,3 +1,26 @@
-const students = [
-    {id:1, name: 'Sanket', email:'ss321@gmail.com'}
-]
+
+// const showBox = () => {
+//     document.querySelector('.notification').style.display = 'block'
+// }
+
+
+
+// setTimeout(() => {
+//     showBox()
+// }, 3000)
+
+// const close = () => {
+//     document.querySelector('.close').addEventListener('click', () => {
+//         document.querySelector('.notification').style.display = 'none'
+//     })
+// }
+
+// close()
+
+const myCK = () => {
+    let dt = new Date()
+    let tm = dt.toLocaleTimeString()
+    document.querySelector('.result').innerHTML = tm
+}
+
+setInterval(myCK, 1000)
